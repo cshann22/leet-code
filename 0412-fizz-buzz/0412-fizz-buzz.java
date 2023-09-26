0215fd80dd482394a6ec800bf7b2b224cd31df8c
Carson Shannon <145373191+cshann22@users.noreply.github.com>
2023-09-26 15:20:33 -0500

    Time: 1 ms (99.9%), Space: 44.5 MB (66.81%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,20 +3,18 @@ class Solution {
 
         ArrayList<String> list = new ArrayList<String>();
 
-        for(int i = 0; i < n; i++){
-            if((i+1) % 5 == 0 && (i+1) % 3 == 0){
+        for(int i = 1; i <= n; i++){
+            if(i % 5 == 0 && i % 3 == 0){
                 list.add("FizzBuzz");
             }
-            else if((i+1) % 3 == 0){
+            else if(i % 3 == 0){
                 list.add("Fizz");
             }
-            else if((i+1) % 5 == 0){
+            else if(i % 5 == 0){
                 list.add("Buzz");
             }
             else{
-                String t = "";
-                t += i + 1;
-                list.add(t);
+                list.add(String.valueOf(i));
             }
         }
         return list;
