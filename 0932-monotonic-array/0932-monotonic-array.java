88bbf85a06ee30ae0975a8fc7ff6439ab128ef36
Carson Shannon <145373191+cshann22@users.noreply.github.com>
2023-09-29 14:56:09 -0500

    Time: 2 ms (62.8%), Space: 55.1 MB (85.02%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int direction = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > nums[i-1]){
+                if(direction == 0){
+                    direction = 1;
+                }
+                else if(direction == -1){
+                    return false;
+                }
+            }
+            else if(nums[i] < nums[i-1]){
+                if(direction == 0){
+                    direction = -1;
+                }
+                else if(direction == 1){
+                    return false;
+                }
+            }
+            
+        }
+
+        return true;
+    }
+}
